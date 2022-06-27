57098deebb5b511b8d05965ac9075a346d8b6036
jrousseau <v4lproik@gmail.com>
2022-06-27 18:27:22 +0200

    Add new private key example

---
+++ examples/new_private_key.go
@@ -0,0 +1,26 @@
+package main
+
+import (
+	"fmt"
+	"github.com/gin-gonic/gin"
+	. "github.com/v4lproik/gin-jwks"
+)
+
+func main() {
+	r := gin.Default()
+
+	builder := NewConfigBuilder()
+	config, err := builder.
+		NewPrivateKey().
+		WithKeyId("my-id").
+		WithKeyLength(2048).
+		Build()
+
+	if err != nil {
+		fmt.Errorf("error generating conf %v", err)
+		return
+	}
+
+	r.GET("/", Jkws(*config))
+	r.Run()
+}
